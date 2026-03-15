b3db3a9c5ac9bb848a99ccdf7e6f1ac05d22c1af
Omar Paredes <omarparedes23@gmail.com>
2026-03-15 18:27:11 +0100

    moni

--- types/index.ts
+++ types/index.ts
@@ -113,9 +113,9 @@ export type Database = {
         Relationships: [];
       };
     };
-    Views: Record<string, never>;
-    Functions: Record<string, never>;
-    Enums: Record<string, never>;
-    CompositeTypes: Record<string, never>;
+    Views: { [_ in never]: never };
+    Functions: { [_ in never]: never };
+    Enums: { [_ in never]: never };
+    CompositeTypes: { [_ in never]: never };
   };
 };

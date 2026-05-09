7f2843f6ba2c999940f8059af872f5bb91b1bfd4
Omar Paredes <omarparedes23@gmail.com>
2026-05-09 17:58:56 +0200

    new2

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "@/types";
 
-/** Cliente Supabase para componentes del lado del cliente (browser) */
+/** Cliente Supabase para componentes del lado del cliente (browser) -- */
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
